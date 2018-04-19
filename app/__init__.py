2f85a4156239040532768077676172ee1e91803f
danieltoo <torresestradadaniel@gmail.com>
2018-04-19 10:57:17 -0500

    make it better

--- app/__init__.py
+++ app/__init__.py
@@ -14,15 +14,15 @@ def notify():
     print ("Alerta Recibida")
     alert = request.json["data"][0]  # Extraer datos de la alerta
     print (alert)
-    inzone = functions.determinateZone(alert["location"]) #Determinar el campus en el que se encuentra
+    inzone = functions.zone.determinateZone(alert["location"]) #Determinar el campus en el que se encuentra
     if (inzone != {} ): 
         print (inzone["idZone"])
-        allTokens = functions.getTokens() # Obtiene todos los tokens de dispositivos
-        devicesOnZone = functions.getDevicesOnZone(inzone["idZone"], allTokens) #Determinar lista de tokens de dispositivos en el campus        
-        devicesNear = functions.getDevicesNear(alert["location"] ,allTokens) # Determina tokens de dispositivos cercanos
-        tokens, devices = functions.clearTokens(devicesNear, devicesOnZone) # revisa tokens repetidos
+        allTokens = functions.tokens.getTokens() # Obtiene todos los tokens de dispositivos
+        devicesOnZone = functions.zone.getDevicesOnZone(inzone["idZone"], allTokens) #Determinar lista de tokens de dispositivos en el campus        
+        devicesNear = functions.devices.getDevicesNear(alert["location"] ,allTokens) # Determina tokens de dispositivos cercanos
+        tokens, devices = functions.tokens.clearTokens(devicesNear, devicesOnZone) # revisa tokens repetidos
         if (len(devices) > 0):
-            functions.sendNotifications(alert, tokens, devices) # Envía notificacionesa dispositivos
+            functions.notifications.sendNotifications(alert, tokens, devices) # Envía notificacionesa dispositivos
         else :
             print("No se encontraron dispositivos en el campus o cercanos a la alerta")
     else :
