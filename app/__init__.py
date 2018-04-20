34a54b5de8a1a1ff4575fd72440ce20e0f037545
danieltoo <torresestradadaniel@gmail.com>
2018-04-20 00:16:10 -0500

    agregué template

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 import os
-from flask import Flask, render_template,jsonify,request,abort
+from flask import Flask, render_template,jsonify,request,abort#
 import functions
+from functions import devices,notifications,roads,tokens,zones
 
 app = Flask(__name__)
 @app.route('/')
@@ -14,15 +15,15 @@ def notify():
     print ("Alerta Recibida")
     alert = request.json["data"][0]  # Extraer datos de la alerta
     print (alert)
-    inzone = functions.zone.determinateZone(alert["location"]) #Determinar el campus en el que se encuentra
+    inzone = zones.determinateZone(alert["location"]) #Determinar el campus en el que se encuentra
     if (inzone != {} ): 
         print (inzone["idZone"])
-        allTokens = functions.tokens.getTokens() # Obtiene todos los tokens de dispositivos
-        devicesOnZone = functions.zone.getDevicesOnZone(inzone["idZone"], allTokens) #Determinar lista de tokens de dispositivos en el campus        
-        devicesNear = functions.devices.getDevicesNear(alert["location"] ,allTokens) # Determina tokens de dispositivos cercanos
-        tokens, devices = functions.tokens.clearTokens(devicesNear, devicesOnZone) # revisa tokens repetidos
+        allTokens = tokens.getTokens() # Obtiene todos los tokens de dispositivos
+        devicesOnZone = devices.getDevicesOnZone(inzone["idZone"], allTokens) #Determinar lista de tokens de dispositivos en el campus        
+        devicesNear = devices.getDevicesNear(alert["location"] ,allTokens) # Determina tokens de dispositivos cercanos
+        tokens, devices = tokens.clearTokens(devicesNear, devicesOnZone) # revisa tokens repetidos
         if (len(devices) > 0):
-            functions.notifications.sendNotifications(alert, tokens, devices) # Envía notificacionesa dispositivos
+            notifications.sendNotifications(alert, tokens, devices) # Envía notificacionesa dispositivos
         else :
             print("No se encontraron dispositivos en el campus o cercanos a la alerta")
     else :
