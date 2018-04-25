61aecdd9f15c18132d7fed3ae55a0346f7c91415
danieltoo <torresestradadaniel@gmail.com>
2018-04-25 15:40:27 -0500

    Cambié /notify

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 import os
 from flask import Flask, render_template,jsonify,request,abort#
 import functions
-from functions import devices,notifications,roads,tokens,zones
+from functions import determinateZone,getTokens,getDevicesOnZone,getDevicesNear,clearTokens,sendNotifications
 
 app = Flask(__name__)
 @app.route('/')
@@ -15,15 +15,15 @@ def notify():
     print ("Alerta Recibida")
     alert = request.json["data"][0]  # Extraer datos de la alerta
     print (alert)
-    inzone = zones.determinateZone(alert["location"]) #Determinar el campus en el que se encuentra
+    inzone = determinateZone(alert["location"]) #Determinar el campus en el que se encuentra
     if (inzone != {} ): 
         print (inzone["idZone"])
-        allTokens = tokens.getTokens() # Obtiene todos los tokens de dispositivos
-        devicesOnZone = devices.getDevicesOnZone(inzone["idZone"], allTokens) #Determinar lista de tokens de dispositivos en el campus        
-        devicesNear = devices.getDevicesNear(alert["location"] ,allTokens) # Determina tokens de dispositivos cercanos
-        tokens, devices = tokens.clearTokens(devicesNear, devicesOnZone) # revisa tokens repetidos
+        allTokens = getTokens() # Obtiene todos los tokens de dispositivos
+        devicesOnZone = getDevicesOnZone(inzone["idZone"], allTokens) #Determinar lista de tokens de dispositivos en el campus        
+        devicesNear = getDevicesNear(alert["location"] ,allTokens) # Determina tokens de dispositivos cercanos
+        tokens, devices = clearTokens(devicesNear, devicesOnZone) # revisa tokens repetidos
         if (len(devices) > 0):
-            notifications.sendNotifications(alert, tokens, devices) # Envía notificacionesa dispositivos
+            sendNotifications(alert, tokens, devices) # Envía notificacionesa dispositivos
         else :
             print("No se encontraron dispositivos en el campus o cercanos a la alerta")
     else :
