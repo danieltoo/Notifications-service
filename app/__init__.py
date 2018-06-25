7c4a81499778e6e65652bd2396e635ce3100f02c
Daniel Torres <torresestradadaniel@gmail.com>
2018-06-24 21:20:53 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,59 @@
 # coding=utf-8
 import os
-from flask import Flask, render_template,jsonify,request,abort
+from flask import Flask, render_template,jsonify,request,abort,redirect,g,url_for
+from flask_socketio import SocketIO, send, emit
+from functools import wraps
+import crypt
+import random
+import datetime
+import config
 from app.Client import SmartClient
 from app.Functions import determinateZone
 from app.Notifications import Notifications
 
 client = SmartClient() 
 noti = Notifications()
+app = Flask(__name__,static_url_path='/static')
+socketio = SocketIO(app)
 
-app = Flask(__name__)
+globalCode = crypt.crypt(config.username, config.password)
+
+@socketio.on('registre')
+def test_registre(body):
+    code = body["code"]
+    if (body["username"] == config.username and body["password"] == config.password) :
+        emit(code, globalCode)
+    else :
+        emit(code, False)
+    return 
+
 @app.route('/')
 def index():
-    #return render_template('./index.html', name = "Your project name") 
-    return jsonify( {"message" : "SmartSecurity Notifications is running"} ),200
+    return render_template('./index.html') 
 
 @app.route('/notify', methods=['POST'])
-def notify(): 
+def notify():  
     print ("Alerta Recibida")
     alert = request.json["data"][0]  # Extraer datos de la alerta
-    print (alert)
+    socketio.emit('alert:' + globalCode, alert, broadcast=True)  #Envía todas las alertas
+
     zones  = client.getZones()
     inzone = determinateZone(alert["location"], zones) #Determinar el campus en el que se encuentra
-    if (inzone != {} ): 
-        print (inzone["idZone"])
-        allTokens = client.getTokens() # Obtiene todos los tokens de dispositivos
-        devicesOnZone = client.getDevicesOnZone(inzone["idZone"]) #Determinar lista de tokens de dispositivos en el campus        
-        devicesNear = client.getDevicesNear(alert["location"]) # Determina tokens de dispositivos cercanos
-        tokens, devices = noti.clearTokens(devicesNear, devicesOnZone, allTokens) # revisa tokens repetidos
 
-        print (devices)
+    if (inzone != {} ): 
+        socketio.emit('alert:' + inzone["idZone"], alert, broadcast=True) #Envía alertas de zona 
+        allTokens = []
+        devicesNear = []
+        tokens = []
+        devices = []
+        devicesOnZone = client.getDevicesOnZone(inzone["idZone"]) #Determinar lista de tokens de dispositivos en el campus        
+        if alert["alertSource"].find("Device_Smartphone_") != -1: #Determinar si la alerta es de Smartphone
+            allTokens = client.getTokens()
+            devicesNear = client.getDevicesNear(alert["location"]) # Determina tokens de dispositivos cercanos
+        else:
+            allTokens = client.getTokens("All")
+        tokens, devices = noti.clearTokens(alert["alertSource"], devicesNear, devicesOnZone, allTokens)
         if (len(devices) > 0):
-            print("OK")
             noti.sendNotifications(alert, tokens, devices) # Envía notificacionesa dispositivos
         else :
             print("No se encontraron dispositivos en el campus o cercanos a la alerta")
@@ -40,7 +63,8 @@ def notify():
 
 
 
-  
+
+
 
     
 
