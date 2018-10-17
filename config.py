954975800f57f64770da6f1a9489b4bdf2523163
Daniel Torres <torresestradadaniel@gmail.com>
2018-10-17 15:36:24 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 
-"""context = "130.206.113.226"
+context = "130.206.113.226"
 smart = "smartsecurity-webservice.herokuapp.com"
 fcm = 'AAAAg2zJvBk:APA91bF5zfWwhVLQjSMF6B5NgIGlKVx3RRwECzX9_RzRJ4UP3lKDDWM9UB1gSXYSvCA1P9wQeyy0JSa0z68eUzpjR9h-YBqamimQ97WuG3cBmuwoCXhSXzIFb7aEp-3Ye1zS1ejuPiQZ'
 username = 'daniel'
 password = "sm2"
-"""
 
+"""
 import os
 context=os.environ['CONTEXT']
 smart="http://"+ os.environ['SMART_SERVICE']+":4005"
 fcm=os.environ['FCM_SERVER_TOKEN']
 username=os.environ['USER_NAME']
 password=os.environ['PASSWORD']
-
+"""
