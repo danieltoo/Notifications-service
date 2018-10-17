053db76ee5a7bbeadb71fe632d591a5df1037af5
Daniel Torres <torresestradadaniel@gmail.com>
2018-10-17 15:38:44 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 context = "130.206.113.226"
-smart = "smartsecurity-webservice.herokuapp.com"
+smart = "https://smartsecurity-webservice.herokuapp.com"
 fcm = 'AAAAg2zJvBk:APA91bF5zfWwhVLQjSMF6B5NgIGlKVx3RRwECzX9_RzRJ4UP3lKDDWM9UB1gSXYSvCA1P9wQeyy0JSa0z68eUzpjR9h-YBqamimQ97WuG3cBmuwoCXhSXzIFb7aEp-3Ye1zS1ejuPiQZ'
 username = 'daniel'
 password = "sm2"
